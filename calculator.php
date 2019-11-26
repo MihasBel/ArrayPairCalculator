d241e8320c0519bbd87b6f075e85b0348e8f5fbd
Mikhail Belyaev <belyaevmsmail@yandex.ru>
2019-11-26 15:19:23 +0300

    Array calculator for test final

--- calculator.php
+++ calculator.php
@@ -4,19 +4,11 @@ $arr = explode(",",$_POST[arr]);
 $pair = 0;
 if (isset($arr[1])) {
     for ($i = 0; $i <= count($arr); $i++) {
-
-
         if ($arr[$i] == $arr[$i - 1]) {
             $pair++;
         }
-
-
     }
     echo "<p class='array-calculator_result'>количество последовательных пар = $pair</p>";
 } else {
     echo "<p class='array-calculator_result'>Минимальная длина массива 2!</p>";
 }
-
-
-
-?>
