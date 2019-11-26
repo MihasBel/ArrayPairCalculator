0468ac87ddbe3d702e6675e6096081ca80865796
Mikhail Belyaev <belyaevmsmail@yandex.ru>
2019-11-26 16:20:02 +0300

    Array calculator for test final

--- TestArray.php
+++ TestArray.php
@@ -7,21 +7,11 @@
 </head>
 <body>
 <main>
-    <section class="banners">
-        <div class="banners_container">
-            <h2 class="banners_title">сохранение пропорций</h2>
-            <div class="banner banner__first"></div>
-        </div>
-        <div class="banners_container">
-            <h2 class="banners_title">покрытие поверхности</h2>
-            <div class="banner banner__second"></div>
-        </div>
-    </section>
     <section class="array-calculator">
         <h2>Вывод количества пар для массива</h2>
         <form class="array-calculator_form" method="post" action="">
             <textarea name="arr" class="array-field" placeholder="Введите массив в формате 1,32,5,n..." >
-Введите массив в формате 1,32,5,n...
+                Введите массив в формате 1,32,5,n...
             </textarea>
             <input name="send" type="submit" class="array-calculator_button" value="посчитать">
         </form>
